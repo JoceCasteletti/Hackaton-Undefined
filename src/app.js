b4ff0b9b267820415f4ed2fc3c1e9db427a5b170
Vilantra <karen.reyes.c@gmail.com>
2019-03-30 15:38:49 -0300

    cards y data

--- src/app.js
+++ src/app.js
@@ -1,1 +1,10 @@
-//aqui lo funcional
+// const dataBase = window.STORES.stores;
+// const filterData = (dataBase) => {
+//   const filter = dataBase.filter(element =>{
+//       return element;
+//   });
+//   return filter;
+// }
+
+
+// window.filterData = filterData;
--- src/main.js
+++ src/main.js
@@ -1,3 +1,23 @@
 const storesData = window.STORES.stores;
+let result = storesData;
+function showData(){  
+    
+for (let i=0; i<storesData.length; i++){
+       //console.log(storesData[i].name);
+document.getElementById('area').innerHTML += ` 
+<div class="col-md-4">
+<div class="card" style="width: 18rem;">
+  <img src="${storesData[i].img}" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">${storesData[i].name}</h5>
+    <p class="card-text">${storesData[i].description}</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div>
+</div>
+</div>`
+}
+};
 
-//console.log(storesData[0].id);
+
+window.onload=showData();
