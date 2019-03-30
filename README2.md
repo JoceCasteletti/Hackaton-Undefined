7780dd410469161c7cf45bbe18dbb1ee01afaf33
JoceCasteletti <47110892+JoceCasteletti@users.noreply.github.com>
2019-03-30 09:32:18 -0300

    Add files via upload

---
+++ README2.md
@@ -0,0 +1,70 @@
+Planificación del proyecto 📑
+
+Historias de usuario 👩🏻🧔🏻💻💡
+
+Historia de usuario 1
+Como: Comprador de juguetes para niños.
+
+Quiero: ver todas las tiendas que existen
+
+Para: poder escoger el de mi preferencia.
+
+CRITERIOS DE ACEPTACIÓN
+
+La aplicación debe tener un botón que le permita al usuario ver todas las tiendas
+
+La aplicación debe mostrar cada tienda con su nombre y una información detallada que le permita al usuario seleccionar el de su preferencia.
+
+Historia de usuario 2
+Como: Usuario de compra de juguetes unisex.
+
+Quiero: ver las tiendas y su ubicación.
+
+Para: que mi busqueda sea más rapida.
+
+CRITERIOS DE ACEPTACIÓN
+
+La app debe tener un botón que al usuario darle click le permita visualizar todas las tiendas
+
+Historia de usuario 3
+Como: Usuario de compras de juguetes
+
+Quiero: Ver las tiendas en la región metropolitana.
+
+Para: saber donde puedo ir a comprar juguetes unisex.
+
+CRITERIOS DE ACEPTACIÓN
+
+La app debe mostrar las tiendas en la región metropolitana
+
+
+Historia de usuario 4
+Como: Usuario de compra de jueguetes.
+
+Quiero: ver una lista de todas las tiendas disponibles
+
+Para: hacer una mejor busqueda al momento de decidir.
+
+CRITERIOS DE ACEPTACIÓN
+
+La aplicación debe tener un botón que le permita al usuario ver todas las tiendas.
+
+La aplicación debe mostrar cada tienda con su nombre y una información detallada que le permita al usuario seleccionar el de su preferencia.
+
+
+Historia de usuario 5
+Como: Comprador de juguetes.
+
+Quiero: saber cuales tiendas son unisex
+
+Para: estar preparado en el momento de iniciar mi juego.
+
+CRITERIOS DE ACEPTACIÓN
+
+La aplicación debe tener un botón que le permita al usuario ver todos las tiendas.
+.
+
+Encuesta aplicada 📋
+Se realizó una encuesta con el Formulario de Google Forms, con el objetivo de conocer un poco más sobre los compradores de juguetes de niños, que fue compartida a través de vaarias redes sociales, recibiendo un total de 20 respuestas.
+
+La encuesta se realizó con Google el cual se pueden encontrar aquí
---
+++ data/stores.js
@@ -0,0 +1,86 @@
+const STORES = {
+  "stores": [{
+    "id": 1,
+    "name": "Cubotopia",
+    "type": [
+      "Destreza",
+      "Habilidad Matematicas",
+      "Conocimiento Espacial",
+      "Retención de Informacion"
+    ],
+    "img": "https://www.cubopia.cl/wp-content/uploads/2019/02/my-shop-logo-1536981855-1.jpg",
+    "web": "https://www.cubopia.cl/",
+    "direction": "Calle San Ignacio de Loyola 75, local 9",
+    "comuna": "Santiago",
+    "fono": +56950955261,
+    "email": "contacto@cubotopia.cl",
+    "facebook": "https://www.facebook.com/cubopia",
+    "instagram": "https://www.instagram.com/cubopia/",
+    "maps": "https://goo.gl/maps/7p7EY9Zae8k",
+    "description": "Tienda de Juguetes, para niños y niñas, dedicada a potenciar las habilidades y destrezas de los niños, mediante juegos como puzzlez, cubos rubik, etc",
+  }, {
+    "id": 2,
+    "name": "PlayBox",
+    "type": [
+      "Destreza",
+      "Curiosidad",
+      "Aprendizaje",
+      "Retención de Informacion"
+    ],
+    "img": "https://www.playbox.cl/wp-content/uploads/2018/04/logo-play-box-pocoyo-300x245.jpg",
+    "web": "https://www.playbox.cl/",
+    "direction": "La Florida 9660",
+    "comuna": "La Florida, Santiago",
+    "fono": 227557224,
+    "email": "contacto@playbox.cl",
+    "facebook": "https://www.facebook.com/PlayBox.de.Ansaldo/",
+    "instagram": "https://www.instagram.com/playboxansaldo/",
+    "maps": "https://goo.gl/maps/rRziGMp48f42",
+    "description": "Play Box de Ansaldo es nuestra cadena de  jugueterías  especializada en la venta de productos  para todas las etapas de crecimiento de los niños. En nuestras tiendas  ofrecemos juguetes con licencias de series infantiles  de televisión, estrenos de cine y marcas top que responden a los gustos de los niños y familia, además juguetes inspirados en el aprendizaje y desarrollados para potenciar la curiosidad, destreza y experiencias del juego sano.",
+  },
+  {
+    "id": 3,
+    "name": "Juegueteria Alemana",
+    "type": [
+      "Destreza",
+      "Curiosidad",
+      "Aprendizaje",
+      "Retención de Informacion"
+    ],
+    "img": "https://payload.cargocollective.com/1/16/542569/8682717/logo_905.jpg",
+    "web": "https://www.ja.cl/",
+    "direction": "Huerfanos 875",
+    "comuna": "Santiago",
+    "fono": 229535097,
+    "email": "contacto@ja.cl",
+    "facebook": "https://www.facebook.com/JugueteriaAlemana",
+    "instagram": "https://www.instagram.com/jugueteriaalemana/",
+    "maps": "https://goo.gl/maps/6g15X5ky5KU2",
+    "description": "Empresa Familiar desde 1960, comerzializadora de juguetes de diferentes tipos como modelismo, juegos de salon y puzzles entre otros",
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+]
+
+
+
+
+
+
+
+
+
+
+
+};;
